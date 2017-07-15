c4820942ae02db624efddd1fda3979d5211576e6
Pierre-Baptiste <pierrebaptistebouillon@gmail.com>
2017-07-15 15:16:13 -0400

    Added jquery-scroll to home call-to-action

--- themes/pb/assets/script/app.js
+++ themes/pb/assets/script/app.js
@@ -3,3 +3,9 @@ $( document ).ready(function(){
     $(".button-collapse").sideNav();
 
 });
+
+$("#explore").click(function() {
+    $('html, body').animate({
+        scrollTop: $('#intro').offset().top - 40
+    }, 800);
+});
