828eda2bb2634f2e28a498ce1a3b22cbf86e6b20
Pierre-Baptiste BOUILLON <p.bouillon@oberthur.com>
2017-07-20 11:56:06 -0400

    Added Lightbox in experience timeline

--- themes/pb/assets/script/app.js
+++ themes/pb/assets/script/app.js
@@ -2,6 +2,15 @@ $( document ).ready(function(){
 
     $(".button-collapse").sideNav();
 
+    lightbox.option({
+        'resizeDuration': 0,
+        'fadeDuration': 20,
+        'disableScrolling': true,
+        'wrapAround': true,
+        'fitImagesInViewport': true,
+        'albumLabel': "%1/%2"
+    })
+
 });
 
 $("#explore").click(function() {
