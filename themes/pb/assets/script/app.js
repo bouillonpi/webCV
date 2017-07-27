470996c0a9883f03bf7b8ebdf6336e16f1d6422e
Pierre-Baptiste BOUILLON <p.bouillon@oberthur.com>
2017-07-27 13:22:39 -0400

    Finished design on Cool block and Contact block

--- themes/pb/assets/script/app.js
+++ themes/pb/assets/script/app.js
@@ -21,3 +21,24 @@ $("#explore").click(function() {
         scrollTop: $('#intro').offset().top - 40
     }, 800);
 });
+
+// https://macarthur.me/typeit
+// https://macarthur.me/typeit/docs
+$('#typeWrite').typeIt({
+    speed: 80,
+    autoStart: false,
+    breakLines: false,
+    deleteDelay: 5000,
+    loop: true,
+    loopDelay: 4000,
+    strings: [
+        'Je vis à <i class="fa fa-tachometer"></i> 100 km/h !',
+        'Je pratique le Kung Fu et le Karaté <i class="fa fa-hand-rock-o"></i>',
+        'Je fais de la détection de métaux <i class="fa fa-magnet"></i>',
+        'Je suis mauvais à League of Legends <i class="fa fa-thumbs-o-down"></i>',
+        'Je fais des blagues pas drôles <i class="fa fa-meh-o"></i>',
+        'Je suis le caméraman de ZeratoR <i class="fa fa-camera"></i>',
+        'Je n\'arrive pas à faire ça : <i class="fa fa-hand-spock-o"></i>',
+        'Mes amis m\'appellent PB'
+    ]
+});
