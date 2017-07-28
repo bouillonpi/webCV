d68583486ab015ed75ebcc68b2c39cb8f20b2ca8
Pierre-Baptiste BOUILLON <p.bouillon@oberthur.com>
2017-07-28 11:55:35 -0400

    Added about-intro block and some translations

--- themes/pb/assets/script/app.js
+++ themes/pb/assets/script/app.js
@@ -16,9 +16,15 @@ $( document ).ready(function(){
 
 });
 
-$("#explore").click(function() {
+$("#home-scroll").click(function() {
     $('html, body').animate({
-        scrollTop: $('#intro').offset().top - 40
+        scrollTop: $('#home-intro').offset().top - 40
+    }, 800);
+});
+
+$("#about-scroll").click(function() {
+    $('html, body').animate({
+        scrollTop: $('#about-intro').offset().top - 40
     }, 800);
 });
 
@@ -51,10 +57,9 @@ $('#typeWriteAbout-FR').typeIt({
     loop: true,
     loopDelay: 4500,
     strings: [
-        'Je vis à <i class="fa fa-tachometer"></i> 100 km/h !',
+        'Je pratique le Kung Fu et le Karaté <i class="fa fa-hand-rock-o"></i>',
         'Je fais des trucs <span class="css-badass">#BADA55</span> en <span class="css-css">#C55</span>',
         'Je regarde le coucher de soleil à Manhattan',
-        'Je pratique le Kung Fu et le Karaté <i class="fa fa-hand-rock-o"></i>',
         'Je suis mauvais à League of Legends <i class="fa fa-thumbs-o-down"></i> </br>(Pourtant je tryhard)',
         'Je fais des blagues bien nulles <i class="fa fa-meh-o"></i>',
         'Je suis le caméraman de ZeratoR <i class="fa fa-camera"></i>',
