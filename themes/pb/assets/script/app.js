1db3eb4f4026898ebfc77447d8ab86bc8cc469dc
Pierre-Baptiste BOUILLON <p.bouillon@oberthur.com>
2017-08-29 16:59:24 -0400

    Case study design work and other minor changes

--- themes/pb/assets/script/app.js
+++ themes/pb/assets/script/app.js
@@ -35,7 +35,7 @@ $('#typeWriteHome-fr').typeIt({
         'Je fais de la détection de métaux <i class="fa fa-magnet"></i>',
         'Je suis mauvais à League of Legends <i class="fa fa-thumbs-o-down"></i>',
         'Je fais des blagues bien nulles <i class="fa fa-meh-o"></i>',
-        'Je suis le caméraman de ZeratoR <i class="fa fa-camera"></i>',
+        'Je suis le caméraman du youtuber ZeratoR <i class="fa fa-camera"></i>',
         'Je n\'arrive pas à faire ça : <i class="fa fa-hand-spock-o"></i>'
     ]
 });
@@ -53,7 +53,7 @@ $('#typeWriteHome-en').typeIt({
         'Je fais de la détection de métaux <i class="fa fa-magnet"></i>',
         'Je suis mauvais à League of Legends <i class="fa fa-thumbs-o-down"></i>',
         'Je fais des blagues bien nulles <i class="fa fa-meh-o"></i>',
-        'Je suis le caméraman de ZeratoR <i class="fa fa-camera"></i>',
+        'Je suis le caméraman du youtuber ZeratoR <i class="fa fa-camera"></i>',
         'Je n\'arrive pas à faire ça : <i class="fa fa-hand-spock-o"></i>'
     ]
 });
@@ -68,16 +68,15 @@ $('#typeWriteAbout-fr').typeIt({
     strings: [
         'Je pratique le Kung Fu et le Karaté <i class="fa fa-hand-rock-o"></i>',
         'Je fais des trucs <span class="css-badass">#BADA55</span> en <span class="css-css">#C55</span>',
-        'Je regarde le coucher de soleil à Manhattan',
+        'Je prends des photos risquées au Grand Canyon',
         'Je suis mauvais à League of Legends <i class="fa fa-thumbs-o-down"></i> </br>(Pourtant je tryhard)',
         'Je fais des blagues bien nulles <i class="fa fa-meh-o"></i>',
-        'Je suis le caméraman de ZeratoR <i class="fa fa-camera"></i>',
+        'Je suis le caméraman du youtuber ZeratoR <i class="fa fa-camera"></i>',
         'Je n\'arrive pas à faire ça : <i class="fa fa-hand-spock-o"></i>',
         'J\'ai essayé de comprendre Prolog. Sans succès...',
         'J\'adore jouer aux Loups-Garous de Thiercelieux avec des amis',
         'Record personnel de Rubik\'s Cube : <i class="fa fa-clock-o"></i> 38s',
-        'Je fais de la détection de métaux <i class="fa fa-magnet"></i>',
-        'Mon chat Ioda pèse 11kg et mesure 1,40m de long. Fat kid.'
+        'Je fais de la détection de métaux <i class="fa fa-magnet"></i>'
     ]
 });
 
@@ -91,16 +90,15 @@ $('#typeWriteAbout-en').typeIt({
     strings: [
         'Je pratique le Kung Fu et le Karaté <i class="fa fa-hand-rock-o"></i>',
         'Je fais des trucs <span class="css-badass">#BADA55</span> en <span class="css-css">#C55</span>',
-        'Je regarde le coucher de soleil à Manhattan',
+        'Je prends des photos risquées au Grand Canyon',
         'Je suis mauvais à League of Legends <i class="fa fa-thumbs-o-down"></i> </br>(Pourtant je tryhard)',
         'Je fais des blagues bien nulles <i class="fa fa-meh-o"></i>',
-        'Je suis le caméraman de ZeratoR <i class="fa fa-camera"></i>',
+        'Je suis le caméraman du youtuber ZeratoR <i class="fa fa-camera"></i>',
         'Je n\'arrive pas à faire ça : <i class="fa fa-hand-spock-o"></i>',
         'J\'ai essayé de comprendre Prolog. Sans succès...',
         'J\'adore jouer aux Loups-Garous de Thiercelieux avec des amis',
         'Record personnel de Rubik\'s Cube : <i class="fa fa-clock-o"></i> 38s',
-        'Je fais de la détection de métaux <i class="fa fa-magnet"></i>',
-        'Mon chat Ioda pèse 11kg et mesure 1,40m de long. Fat kid.'
+        'Je fais de la détection de métaux <i class="fa fa-magnet"></i>'
     ]
 });
 
